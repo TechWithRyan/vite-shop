109ce75144413db0ad0d7c31d0b686d8e58fe15e
Ryan-Phillips Iserhills Cornelio <ryan.cornelio@gmail.com>
2022-12-10 10:55:54 +0100

    Styles of homepage and cart

---
+++ server/config/dbconfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+    HOST: "localhost";
+    USER: 'root';
+    PASSWORD: '1291@Mysql';
+    DB: 'post';
+    dialect: 'Mysql';
+    pool: {
+        max: 5,
+        min: 0
+        accuire: 30000,
+        idle: 10000
+    }
+};
+
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,26 +1,33 @@
 import React from 'react'
-import '../App.css' 
+import '../App.css'
 import A1 from '../../images/A1.jpeg'
 
 function Cart() {
   return (
 
     <div className="cart-container">
-        <div className="cart-wrapper">
-          <div className="cart-title">Cart</div>
-          <div className="cart-item">
-            <div className="cart-thumbnail">
-            <img src={A1}  />
-              </div>
-                <div className="main-container">
-
-                <div className="cart-product">Shoes</div>
-                <div className="cart-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit ratione excepturi blanditiis fugit delectus, quasi obcaecati eius sunt ad sed illum nam distinctio voluptates, in quos? Enim velit quo a. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut, labore. Voluptatum odit at iusto eos, illum doloribus distinctio eligendi repellendus nesciunt iste quo recusandae perspiciatis, consectetur qui necessitatibus iure fugiat?</div>
-                <div className="cart-price">$200</div>
-              </div>
+      <div className="cart-wrapper">
+        <div className="cart-title">Cart</div>
+        <div className="cart-item">
+          <div className="cart-thumbnail">
+            <img src={A1} />
           </div>
-            
+          <div className="main-container">
+            <div className="cart-product">Shoes</div>
+            <div className="cart-description">Lorem ipsum dolor, sit amet
+              consectetur adipisicing elit. Sit ratione excepturi blanditiis
+              fugit delectus, quasi obcaecati eius sunt ad sed illum nam
+              distinctio voluptates, in quos? Enim velit quo a. Lorem
+              ipsum, dolor sit amet consectetur adipisicing elit. Ut,
+              labore. Voluptatum odit at iusto eos, illum doloribus
+              distinctio eligendi repellendus nesciunt iste quo recusandae
+              perspiciatis, consectetur qui necessitatibus iure fugiat?</div>
+            <div className="cart-price">$200</div>
+          </div>
+
         </div>
+
+      </div>
     </div>
   )
 }
