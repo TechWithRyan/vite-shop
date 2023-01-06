6cac7d7ce470c9760139d06c675889cfecdd7c01
Ryan-Phillips Iserhills Cornelio <ryan.cornelio@gmail.com>
2023-01-06 13:55:58 +0100

    fix

--- src/component/about.jsx
+++ src/component/about.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import Card from '@mui/material/Card';
-import Ryan from '../../images/ryan.jpg'
 import Me1 from '../../images/Me1.jpg'
-import Swe from '../../images/flag-swe.png'
-import Ph from '../../images/ph-flag.jpg'
 import Max from '../../images/ryan.jpg'
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -18,8 +15,6 @@ function About() {
             <Grid className="grid-style">
                 <div class="about-container">
                     <img src={Me1} className="myphoto"></img>
-                    {/* <img src={Swe} className="myphoto"></img>
-                    <img src={Ph} className="myphoto"></img> */}
                     <h1>Ryan-Phillips Iserhills Cornelio </h1>
                     <i>Web development in E-commerce</i>
                     <br />
